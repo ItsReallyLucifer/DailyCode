e1e185d203e1f92cad1b00b149e9d76adf25ae27
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-03-29 12:31:34 -0700

    check if num is even

---
+++ level8/EvenOrNot/main.js
@@ -0,0 +1,15 @@
+// create a function that determines whether or not a number is even.
+
+
+
+
+function testEven(n) {
+    if (n % 2 === 0) {
+      return true;
+    }else{
+      return false
+    }
+  }
+
+  console.log(testEven(5)) // false
+  console.log(testEven(6)) // true
