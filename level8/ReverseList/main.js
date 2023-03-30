88953a2d2449add1f8a5a50944885a63ba96ae5b
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-03-30 12:38:23 -0700

    reverse list param

---
+++ level8/ReverseList/main.js
@@ -0,0 +1,11 @@
+// create a function that takes in a list and returns a list with the reverse order.
+
+
+
+function reverseList(list) {
+    return list.reverse();
+  }
+
+ const order = [1,2,3,4]
+ const reversed = reverseList(order)
+ console.log(reversed) // [4,3,2,1]
