a37f4782813e3b2365be1b40fa51ac2069047c22
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-03-31 12:44:19 -0700

    reverse digits store in array

---
+++ level8/ReverseDigits/main.js
@@ -0,0 +1,11 @@
+// Given a random non-negative number, 
+//you have to return the digits of this number within an array in reverse order.
+
+
+
+
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+  }
+
+  console.log(digitize(12345)) // [5, 4, 3, 2, 1]
