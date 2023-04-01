475fe52fbd09fd9f4350c9e30954adbee208c8ee
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-04-01 12:50:35 -0700

    switchcase nums to str

---
+++ level8/SwitchcaseWords/main.js
@@ -0,0 +1,51 @@
+// using switchcase, write a function that returns -
+// number between 0-9 - in words.
+// input 1 --> One
+
+
+function switchItUp(number) {
+    switch (number) {
+      case 0:
+        return "Zero";
+      case 1:
+        return "One";
+      case 2:
+        return "Two";
+      case 3:
+        return "Three";
+      case 4:
+        return "Four";
+      case 5:
+        return "Five";
+      case 6:
+        return "Six";
+      case 7:
+        return "Seven";
+      case 8:
+        return "Eight";
+      case 9:
+        return "Nine";
+      default:
+        return "Invalid input"; 
+    }
+  }
+
+  console.log(switchItUp(5))
+  
+
+  // or
+
+//   function switchItUp(number) {
+//     switch (number) {
+//       case 0: return 'Zero';
+//       case 1: return 'One';
+//       case 2: return 'Two';
+//       case 3: return 'Three';
+//       case 4: return 'Four';
+//       case 5: return 'Five';
+//       case 6: return 'Six';
+//       case 7: return 'Seven';
+//       case 8: return 'Eight';
+//       case 9: return 'Nine';
+//     }
+//   }
