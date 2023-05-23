13713fb7dd2eb61f5e970cb5bc255fe02bc5cbcd
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-05-23 09:59:28 -0700

    daily update

---
+++ level7/X&O/main.js
@@ -0,0 +1,14 @@
+// Create a function that checks if a string has the same amount of 'X's and 'O's
+// Must return a boolean and be case insensitive.
+// The string can contain any char.
+// example: 'xxo' = false --- 'xoxo' = true
+
+
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+  }
+
+  console.log(XO('xoxoxoxo')) // true
+  console.log(XO('xoxxxo')) // false
