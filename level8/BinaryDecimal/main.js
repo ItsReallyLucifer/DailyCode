e7fe734376a7e17ec90be4d82a859b271590b244
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-05-31 15:56:06 -0700

    daily update

---
+++ level8/BinaryDecimal/main.js
@@ -0,0 +1,8 @@
+// Create a function that converts binary number (given as a string) -
+// and converts it to a decimal number
+
+
+function binToDec(bin){
+    let convert = parseInt(bin,2)
+    return convert
+}
