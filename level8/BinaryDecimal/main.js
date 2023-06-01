b418137a22bfcec9e3c5e1b0d6eca7b0ab714307
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-06-01 16:00:08 -0700

    consolelog addition

--- level8/BinaryDecimal/main.js
+++ level8/BinaryDecimal/main.js
@@ -1,8 +1,11 @@
 // Create a function that converts binary number (given as a string) -
-// and converts it to a decimal number
+// and converts it to a number
 
 
 function binToDec(bin){
     let convert = parseInt(bin,2)
     return convert
 }
+
+console.log(binToDec('110100')) // 52
+console.log(binToDec('1010001')) // 81
