b619337e815176da0d10d2e32d94e7dc38178c9f
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-06-05 12:20:00 -0700

    added call

--- level8/PipeProblem/main.js
+++ level8/PipeProblem/main.js
@@ -1,5 +1,6 @@
 // Given the a list of unique numbers sorted in ascending order, 
 //return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+// example: 1, 3, 5 =  1, 2, 3, 4, 5
 
 
 function pipeFix(numbers){
@@ -12,3 +13,7 @@ function pipeFix(numbers){
     }
     return arr;
   }
+    const input = [1,2,4,7]
+    const output = pipeFix(input)
+
+    console.log(output)
