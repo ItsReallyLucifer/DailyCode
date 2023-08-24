72587a9432fab29814e57b741981b2a356a2daee
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-08-24 15:14:52 -0700

    count divisors of num

---
+++ level7/CountDivisors/main.js
@@ -0,0 +1,18 @@
+// Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000
+// example: 4 --> 3 // we have 3 divisors - 1, 2 and 4
+//          5 --> 2 // we have 2 divisors - 1 and 5
+//         12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+
+
+function getDivisorsCnt(n){
+    let count=0;
+ for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+      count++;
+        }
+    }
+    return count;
+}
+
+console.log(getDivisorsCnt(4))
