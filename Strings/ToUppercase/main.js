0858a066b650417893dc290aec270e3864f7a800
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-09-29 18:17:01 -0700

    UpperCase complete str

---
+++ Strings/ToUppercase/main.js
@@ -0,0 +1,10 @@
+// Create a function which converts the input string to Uppercase
+
+
+
+function makeUpperCase(str) {
+    return str.toLocaleUpperCase();
+  }
+
+console.log(makeUpperCase('Did it work?')) // DID IT WORK?
+
