8672d8ba1d45aa8ff54364660753f6e880b242f2
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-10-06 10:43:17 -0700

    invert num vals

---
+++ Arrays/InvertVal/main.js
@@ -0,0 +1,10 @@
+//Create a function that takes an array of numbers as input 
+//and returns a new array where each element's sign is inverted -
+//(positive numbers become negative, and negative numbers become positive), without mutating the input array.
+
+
+function invert(arr){
+    return arr.map((num) => -num)
+}
+console.log(invert([1,2,3,4,5])) // -1, -2, -3, -4, -5
+console.log(invert([-5,-7,-10])) // 5, 7, 10
